3c3a9a5c9aa0122ce51ff6186ea81a45319dc3d7
samarth-patel <samarth.patel@zoominfo.com>
2022-10-13 15:31:28 -0400

    Mock data and service file

---
+++ PowerModeApp/src/app/Data/contact-mock-data.ts
@@ -0,0 +1,81 @@
+export const Contacts = [
+  {
+    OrgId: 1075744327681194,
+    OwnerId: 1075763128812880,
+    Name: "Aaron Fuller",
+    Company: "Walmart",
+    Email: "aaron.fuller@wal-mart.com",
+    Id: 1134212788101171,
+    Salesflows: ["102500_200"]
+  },
+
+ {
+    OrgId: 1075744327681194,
+    OwnerId: 1075763128812880,
+    Name: "Richard Mayfield",
+    Company: "Walmart",
+    Email: "richard.mayfield@wal-mart.com",
+    Id: 1090179787459205,
+    Salesflows: ["102500_50", "93057_200", "93056_200", "92894_200", "92886_300"]
+  },
+
+  {
+    OrgId: 1075744327681194,
+    OwnerId: 1075763128812880,
+    Name: "Adarsh Kumar",
+    Company: "Walmart",
+    Email: "adarsh.kumar@wal-mart.com",
+    Id: 1134212788072466,
+    Salesflows: []
+  },
+
+  {
+    OrgId: 1075744327681194,
+    Id: 1134212788051973,
+    OwnerId: 1075763128812880,
+    Name: "Alex Krueger",
+    Company: "Walmart",
+    Email: "alex.krueger@wal-mart.com",
+    Salesflows: []
+  },
+
+  {
+    Id: 1134212788084767,
+    OrgId: 1075744327681194,
+    OwnerId: 1075763128812880,
+    Company: "Walmart",
+    Email:"amanda.jenkins@wal-mart.com",
+    Name:"Amanda Jenkins",
+    Salesflows: []
+  },
+
+  {
+    Id: 1134212788092974,
+    OrgId:1075744327681194,
+    OwnerId:1075763128812880,
+    Company: "Walmart",
+    Email:"angela.koo@wal-mart.com",
+    Name: "Angela Koo",
+    Salesflows: [93057_200]
+  },
+
+  {
+    Id:1134212788031720,
+    OrgId:1075744327681194,
+    OwnerId:1075763128812880,
+    Name:"Anthony Hucker",
+    Email:"anthony.hucker@wal-mart.com",
+    Company: "Walmart",
+    Salesflows: []
+  },
+
+  {
+    Id:1134212788023522,
+    OrgId:1075744327681194,
+    OwnerId:1075763128812880,
+    Name:"Anuj Sahai",
+    Email:"anuj.sahai@walmartlabs.com",
+    Company: "Walmart",
+    Salesflows: []
+  }
+]
