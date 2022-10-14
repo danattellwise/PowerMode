049cca2ec7e280b7b369a0c2f2eb47b43ac075c7
Dan Do <dan@tellwise.com>
2022-10-14 13:25:41 -0700

    dfasoj

--- PowerModeApp/src/app/app.component.ts
+++ PowerModeApp/src/app/app.component.ts
@@ -10,6 +10,13 @@ import { Router } from '@angular/router';
 })
 
 export class AppComponent {
+  isRoute1 = true;
+  isRoute2 = false;
+  isRoutePowerMode = false;
+  isRouteLogin = false;
+
+
+
   constructor(private router: Router) {
     window.addEventListener("keydown", function(e) {
       if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
@@ -32,19 +39,31 @@ export class AppComponent {
 
   @HostListener('document:keypress', ['$event'])
   navigate(event: KeyboardEvent) {
+    console.log('event', event);
     switch(event.key){
       case '1':
         this.router.navigateByUrl('/contacts');
+        this.isRoute1 = true;
+        this.isRoute2 = false;
+        this.isRoutePowerMode = false;
+        this.isRouteLogin =false;
         break;
 
       case '2':
         this.router.navigateByUrl('/salesflows');
+        this.isRoute1 = false;
+        this.isRoute2 = true;
+        this.isRoutePowerMode = false;
+        this.isRouteLogin =false;
         break;
 
       case '5':
         this.router.navigateByUrl('/login');
+        this.isRoute1 = false;
+        this.isRoute2 = false;
+        this.isRoutePowerMode = false;
+        this.isRouteLogin =true;
         break;
-
 
       case '`':
         // This toggles the legend overlay
