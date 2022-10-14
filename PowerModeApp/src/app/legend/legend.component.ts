cbe33a0216d0af9638da9efd3f73400dc7632e31
allen-westgate <106835959+allen-westgate@users.noreply.github.com>
2022-10-14 14:54:01 -0400

    dynamic legend updates

--- PowerModeApp/src/app/legend/legend.component.ts
+++ PowerModeApp/src/app/legend/legend.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output } from '@angular/core';
+import { KeyBindService } from '../Service/key-bind.service';
 
 @Component({
   selector: 'app-legend',
@@ -6,8 +7,44 @@ import { Component, OnInit, Output } from '@angular/core';
   styleUrls: ['./legend.component.scss']
 })
 export class LegendComponent implements OnInit {
-  constructor() { }
+  keyMap: any = {
+    q: {
+      actionType: 'test',   // ADD_TO_SALESFLOW
+      resourceId: null,
+      resourceName: null
+    },
+    w: {
+      actionType: null,
+      resourceId: null,
+      resourceName: null
+    },
+    e: {
+      actionType: null,
+      resourceId: null,
+      resourceName: null
+    },
+    r: {
+      actionType: null,
+      resourceId: null,
+      resourceName: null
+    },
+  };
+
+  constructor(
+    private keybindService:KeyBindService
+  ) { }
 
   ngOnInit(): void {
+    this.updateLegend();
+  }
+
+  updateLegend() {
+    this.keyMap = this.keybindService.getKeyBinds();
+    const keys = Object.entries(this.keyMap);
+
+    keys.forEach(key => {
+      if(key[0] == null)
+        key[0] = 'Unassigned';
+    })
   }
 }
