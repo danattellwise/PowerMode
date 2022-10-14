03b9116c387b9ca883ea646c7dcd425029ca9ec7
allen-westgate <106835959+allen-westgate@users.noreply.github.com>
2022-10-14 13:44:13 -0400

    steal dan and priyanka's hard work

--- PowerModeApp/src/app/power-mode/power-mode.component.ts
+++ PowerModeApp/src/app/power-mode/power-mode.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 import { Contacts } from '../Data/contact-mock-data';
+// import { PowerModeService } from '../Service/power-mode.service';
 
 @Component({
   selector: 'app-power-mode',
@@ -7,12 +9,66 @@ import { Contacts } from '../Data/contact-mock-data';
   styleUrls: ['./power-mode.component.scss']
 })
 export class PowerModeComponent implements OnInit {
+currentIndex = 0;
 contacts = Contacts;
-selectedContact = Contacts[0];
+selectedContacts = Contacts;
+selectedContact = this.selectedContacts[0];
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    // private powerModeService: PowerModeService
+    ) { }
 
   ngOnInit(): void {
+    // this.powerModeService.getContactsByChips('companies', 'walmart', 1)
+    //   .subscribe(res => {
+    //     this.contacts = res.Contacts;
+    //   });
   }
 
+  ngAfterViewInit(): void {
+    this.addHighlightClass(this.currentIndex);
+  }
+  updateContactDetails() {
+    this.selectedContact = this.contacts[this.currentIndex];
+  }
+
+  addHighlightClass(i: any) {
+    let elm = this.document.getElementById('cr' + i);
+    elm?.classList.add('highlight');
+    this.updateContactDetails();
+  }
+
+  removeHighlightClass(i: any) {
+    let elm = this.document.getElementById('cr' + i);
+    elm?.classList.remove('highlight');
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    switch (event.key) {
+      case 'ArrowDown':
+        this.removeHighlightClass(this.currentIndex);
+        if (this.currentIndex >= this.contacts.length - 1) {
+          this.currentIndex = 0;
+        } else {
+          this.currentIndex++;
+        }
+        this.addHighlightClass(this.currentIndex);
+        break;
+
+      case 'ArrowUp':
+        this.removeHighlightClass(this.currentIndex);
+        if (this.currentIndex <= 0) {
+          this.currentIndex = this.contacts.length - 1;
+        } else {
+          this.currentIndex--;
+        }
+        this.addHighlightClass(this.currentIndex);
+        break;
+
+      default:
+        break;
+    }
+  }
 }
