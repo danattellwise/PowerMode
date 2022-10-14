cf1da38952a221896985fdddbad69f21b7ed1762
Dan Do <dan@tellwise.com>
2022-10-13 22:25:01 -0700

    use real data for salesflow

--- PowerModeApp/src/app/salesflow-page/salesflow-page.component.ts
+++ PowerModeApp/src/app/salesflow-page/salesflow-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {SalesflowService} from "../services/salesflow.service";
 import {SalesFlow} from "../contracts/salesflow";
 
@@ -8,8 +8,12 @@ import {SalesFlow} from "../contracts/salesflow";
   styleUrls: ['./salesflow-page.component.scss']
 })
 export class SalesflowPageComponent implements OnInit {
-
-  salesflows: any;
+  loading = false;
+  salesflows: any = [];
+  salesflowsToShow: any = [];
+  currentPage = 1;
+  maxPage = 1;
+  pageSize = 20;
 
   constructor(private salesflowService: SalesflowService) { }
 
@@ -18,8 +22,15 @@ export class SalesflowPageComponent implements OnInit {
   }
 
   initializeSalesflows() {
-    this.salesflowService.getSalesflowData().subscribe((res:SalesFlow[]) => {
+    this.loading = true;
+    this.salesflowService.getSalesflows().subscribe((res:SalesFlow[]) => {
      this.salesflows = res;
+     for(let i = 0; i < this.salesflows.length; i++) {
+       this.salesflows[i].Row = i + 1;
+     }
+     this.maxPage = Math.ceil(this.salesflows.length / this.pageSize);
+     this.updateSalesflowsPage();
+     this.loading = false;
     })
   }
 
@@ -33,5 +44,62 @@ export class SalesflowPageComponent implements OnInit {
 
   toggleCheckbox(salesflow: any) {
     salesflow.checked = !salesflow.checked;
+  }
+
+  updateSalesflowsPage() {
+    const start = (this.currentPage - 1) * this.pageSize;
+    const end = this.currentPage * this.pageSize;
+    this.salesflowsToShow = this.salesflows.slice(start, end);
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log('===-=====', event);
+    switch (event.key) {
+      // TODO: Implement for salesflow page
+      // case 'ArrowDown':
+      //   this.removeHighlightClass(this.currentIndex);
+      //   if (this.currentIndex >= this.contacts.length - 1) {
+      //     this.currentIndex = 0;
+      //   } else {
+      //     this.currentIndex++;
+      //   }
+      //   this.addHighlightClass(this.currentIndex);
+      //   break;
+      //
+      // case 'ArrowUp':
+      //   this.removeHighlightClass(this.currentIndex);
+      //   if (this.currentIndex <= 0) {
+      //     this.currentIndex = this.contacts.length - 1;
+      //   } else {
+      //     this.currentIndex--;
+      //   }
+      //   this.addHighlightClass(this.currentIndex);
+      //   break;
+      //
+      // case ' ':
+      //   this.contacts[this.currentIndex].checked = !this.contacts[this.currentIndex].checked;
+      //   break;
+      case 'ArrowLeft':
+        if (this.currentPage == 1) {
+          break;
+        } else {
+          this.currentPage--;
+        }
+        this.updateSalesflowsPage();
+        break;
+
+      case 'ArrowRight':
+        if (this.currentPage == this.maxPage) {
+          break;
+        } else {
+          this.currentPage++;
+        }
+        this.updateSalesflowsPage();
+        break;
+
+      default:
+        break;
+    }
   }
 }
--- PowerModeApp/src/app/services/salesflow.service.ts
+++ PowerModeApp/src/app/services/salesflow.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core'; 
 import { Observable, of } from 'rxjs'
 import {data}  from '../../assets/salesflowData'
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SalesflowService {
  
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getSalesflowData(): Observable<any>{
     return of(data);
+  }
+
+  getSalesflows() {
+    return this.http.get<any>('https://engage-app-staging.zoominfo.com/rest/v1/workflow_templates/dashboard');
   }
 }
